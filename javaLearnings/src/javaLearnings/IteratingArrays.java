30b8bc41508b97c7627aedd4f83c317bedeb9057
rajumnaveenk <naveen.kumar97044@gmail.com>
2025-08-16 19:32:13 +0530

    IteratingArrays
    IteratingArrays

---
+++ javaLearnings/src/javaLearnings/IteratingArrays.java
@@ -0,0 +1,15 @@
+package javaLearnings;
+
+public class IteratingArrays {
+
+	public static void main(String[] args) {
+
+		int[] numbers = { 10, 20, 30, 40, 50 };
+
+		for (int num : numbers) {
+			System.out.println("Iteration: " + num);
+		}
+
+	}
+
+}
