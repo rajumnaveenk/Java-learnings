f22dade628b396c86535f823a24de7a5041292c1
rajumnaveenk <naveen.kumar97044@gmail.com>
2025-08-16 19:03:32 +0530

    ArrayExample2

---
+++ javaLearnings/src/javaLearnings/ArrayExample2.java
@@ -0,0 +1,16 @@
+package javaLearnings;
+
+public class ArrayExample2 {
+
+	public static void main(String[] args) {
+
+		int[] numbers = { 1, 2, 3, 4, 5 };
+		System.out.println("Iteration ");
+
+		for (int num : numbers) {
+			System.out.println("Interation " + num);
+		}
+
+	}
+
+}
