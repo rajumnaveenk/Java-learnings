bc59399b5ec5fba2adea40408f3530a483dce295
rajumnaveenk <naveen.kumar97044@gmail.com>
2025-08-16 18:55:40 +0530

    ArrayExample
    ArrayExample

---
+++ javaLearnings/src/javaLearnings/ArrayExample.java
@@ -0,0 +1,14 @@
+package javaLearnings;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		int[] numbers = { 1, 2, 3, 4, 5 };
+		System.out.println("Iterating using for loop:");
+
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println("Element at index " + i + ": " + numbers[i]);
+		}
+	}
+
+}
