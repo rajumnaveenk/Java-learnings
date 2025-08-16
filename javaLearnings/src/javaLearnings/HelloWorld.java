e337ccf9378f45c2fefe7b032dba805e68c5ad52
rajumnaveenk <naveen.kumar97044@gmail.com>
2025-08-16 14:57:09 +0530

    method code committed

---
+++ javaLearnings/src/javaLearnings/HelloWorld.java
@@ -0,0 +1,13 @@
+package javaLearnings;
+
+public class HelloWorld {
+
+	static void greet() {
+		System.out.println("Hello world!!");
+	}
+
+	public static void main(String[] args) {
+		greet();
+	}
+
+}
