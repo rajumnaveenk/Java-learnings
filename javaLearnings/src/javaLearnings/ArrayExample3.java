b8dd1df0eb017008cef47a5970b20494e79f72fb
rajumnaveenk <naveen.kumar97044@gmail.com>
2025-08-16 19:11:26 +0530

    ArrayExample3

---
+++ javaLearnings/src/javaLearnings/ArrayExample3.java
@@ -0,0 +1,11 @@
+package javaLearnings;
+
+import java.util.Arrays;
+
+public class ArrayExample3 {
+	public static void main(String[] args) {
+		int[] numbers = { 10, 20, 30, 40, 50 };
+
+		System.out.println("Array contents: " + Arrays.toString(numbers));
+	}
+}
