ab286411be9383e47b3931fcf0bf4fc5deb97c35
rajumnaveenk <naveen.kumar97044@gmail.com>
2025-08-16 15:23:51 +0530

    Animal committ

---
+++ javaLearnings/src/javaLearnings/Animals.java
@@ -0,0 +1,14 @@
+package javaLearnings;
+
+public class Animals {
+
+	public void Dog(String name) {
+		System.out.println("my Dog is " + name);
+	}
+
+	public static void main(String[] args) {
+		Animals a = new Animals();
+		a.Dog("Raju");
+	}
+
+}
